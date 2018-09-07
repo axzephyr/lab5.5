740f79d5df7969a6c4a31da5186a00cff7951055
axzephyr <42173172+axzephyr@users.noreply.github.com>
2018-09-07 10:39:51 +0530

    Update lab5.5q2.cpp

--- lab5.5q2.cpp
+++ lab5.5q2.cpp
@@ -4,11 +4,15 @@ using namespace std;
 //start the main function
 int main()
 {
+    int n;
+    //ask the user for the number of lines it wants
+    cout<<" how many lines do you want? "<<endl;
+    cin>>n;
 	//start the loop for the star line
-	for(int i=0;i<9;i++)
+	for(int i=0;i<n;i++)
 	{
 	//put in the conditions for the starry line( first and last)
-	if(i==0||i==8)
+	if(i==0||i==(n-1))
 		{
 		int j=0;
 		while(j<5)
@@ -17,32 +21,31 @@ int main()
 			j++;
 		}
 	cout<<endl;
-	}	
+	}
 		// put in the conditions for the rest of the lines
 		else
 		{
 			int j=0;
 			for(;j<5;)
 			{
-				//put in the condition for the first and last charecter of the line as a star
-				if(j==0||j==4) 
+				//put in the condition for the first and last charector of the line as a star
+				if(j==0||j==4)
 				{
 				cout<<"*";
 				}
-				// the conditions for the rest charecters to be a blank space
+				// the conditions for the rest charectors to be a blank space
 				else
 				{
 				cout<<" ";
 				}
 			//increase the value of j
-			j++;				
+			j++;
 			}
-		cout<<endl;		     	
+		cout<<endl;
 		}
-	
+
 	}
-//terminate the program	
+//terminate the program
 
 return 0;
-}	
- 
+}
