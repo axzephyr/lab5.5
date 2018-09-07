731428a48e307c8182052d7b4f9c0e66d7103012
axzephyr <42173172+axzephyr@users.noreply.github.com>
2018-09-07 11:29:29 +0530

    Update lab5.5q9.cpp

--- lab5.5q9.cpp
+++ lab5.5q9.cpp
@@ -1,36 +1,33 @@
+//include the library
 #include<iostream>
 using namespace std;
-int main(){
-	for(int i=0;i<5;)
-	{		
-		if(i==0||i==4)
-		{
-			int j=0;
-			while(j<=i)
-			{
-				cout<<"*";
-				j++;
-			}
-		}
-		else
-		{
-			for (int j=0;j<i;)
-			{
-				if (j==0||j==(i-1))
-				{
-					cout<<"*";
-					j++;				
-				}
-				else 
-				{
-					cout<<" ";
-				}
-			}
-		cout<<endl;
-		i++;
-		}
-	}
-return 0;
-}													
-
-		
+//initialize the main program
+int main()
+{
+    //declare the variable for the number of lines
+    int n;
+    //ask the user for the number of lines it wants
+    cout<<" How many lines do you want? "<<endl;
+    cin>>n;
+    //start the looping for the lines
+    for(int i=0;i<=n;i++)
+    {
+        //declare the variable for each of the charector type
+        for(int j=0;j<=i;j++)
+        {
+            //put in the conditions for the charector to be a star or a space
+            if(j==0||j==i||i==n)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        //terminate the line
+        cout<<endl;
+    }
+    //terminate the program
+    return 0;
+}
