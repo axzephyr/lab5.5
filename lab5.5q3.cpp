3ff943ccc6e16ff3e867913f78158a6d649ead46
axzephyr <42173172+axzephyr@users.noreply.github.com>
2018-09-07 10:34:27 +0530

    Update lab5.5q3.cpp

--- lab5.5q3.cpp
+++ lab5.5q3.cpp
@@ -4,20 +4,24 @@ using namespace std;
 //introduce the main function
 int main()
 {
-//put in the variables that control the stars
-	for(int i=9;i>=4;)
+    int n;
+    //ask the user for the number of lines it wants
+    cout<<" how many lines do you want? "<<endl;
+    cin>>n;
+    //put in the variables that control the stars
+	for(int i=0;i<=n;)
 	{
 		//put in the conditions that control the spaces
 		int j=0;
-		while(j<=(i-4))
+		while(j<=(n-i))
 		{
 			//put in the spaces
 			cout<<" ";
 			j++;
 		}
-	i--;
+	i++;
 	//print the stars
 	cout<<"*****"<<endl;
 	}
 return 0;
-}	
+}
